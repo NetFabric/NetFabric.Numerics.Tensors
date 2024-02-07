cba74c43d8abadaa33c73c82d3b903cf20a64b63
Antão Almada <antao.almada@farfetch.com>
2024-02-07 23:02:02 +0000

    Add aggregate operations ProductOfAdditions and ProductOfSubtractions

---
+++ src/NetFabric.Numerics.Tensors.UnitTests/ProductOfAdditionsTests.cs
@@ -0,0 +1,52 @@
+﻿namespace NetFabric.Numerics.Tensors.UnitTests;
+
+public class ProductOfAdditionsTests
+{
+    public static TheoryData<int> ProductOfAdditionsData
+        => new() { 
+            { 0 }, { 1 }, { 2 }, { 3 }, { 4 }, { 5 }, { 6 }, { 7 }, { 8 }, { 9 }, { 10 }, { 100 },
+        };
+
+    static void ProductOfAdditions_Should_Succeed<T>(int count)
+        where T : struct, INumber<T>
+    {
+        // arrange
+        var x = new T[count];
+        var y = new T[count];
+        var expected = T.One;
+        var random = new Random(42);
+        for (var index = 0; index < x.Length; index++)
+        {
+            var value = T.CreateChecked(random.Next(10));
+            x[index] = value;
+            y[index] = value + T.CreateChecked(1);
+            expected *= value + value + T.CreateChecked(1);
+        }
+
+        // act
+        var result = Tensor.ProductOfAdditions<T>(x, y);
+
+        // assert
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(ProductOfAdditionsData))]
+    public void ProductOfAdditions_Int_Should_Succeed(int count)
+        => ProductOfAdditions_Should_Succeed<int>(count);
+
+    [Theory]
+    [MemberData(nameof(ProductOfAdditionsData))]
+    public void ProductOfAdditions_Long_Should_Succeed(int count)
+        => ProductOfAdditions_Should_Succeed<long>(count);
+
+    // [Theory]
+    // [MemberData(nameof(ProductOfAdditionsData))]
+    // public void ProductOfAdditions_Float_Should_Succeed(int count)
+    //     => ProductOfAdditions_Should_Succeed<float>(count);
+
+    // [Theory]
+    // [MemberData(nameof(ProductOfAdditionsData))]
+    // public void ProductOfAdditions_Double_Should_Succeed(int count)
+    //     => ProductOfAdditions_Should_Succeed<double>(count);
+}
--- src/NetFabric.Numerics.Tensors/Operations/Product.cs
+++ src/NetFabric.Numerics.Tensors/Operations/Product.cs
@@ -17,4 +17,37 @@ public static partial class Tensor
     public static ValueTuple<T, T, T, T> Product4D<T>(ReadOnlySpan<T> source)
         where T : struct, IMultiplicativeIdentity<T, T>, IMultiplyOperators<T, T, T>
         => Aggregate4D<T, ProductOperator<T>>(source);
+
+    public static T ProductOfAdditions<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate<T, T, T, AddOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T> ProductOfAdditions2D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate2D<T, T, T, AddOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T, T> ProductOfAdditions3D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate3D<T, T, T, AddOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T, T, T> ProductOfAdditions4D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, IAdditionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate4D<T, T, T, AddOperator<T>, ProductOperator<T>>(x, y);
+
+    public static T ProductOfSubtractions<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, ISubtractionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate<T, T, T, SubtractOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T> ProductOfSubtractions2D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, ISubtractionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate2D<T, T, T, SubtractOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T, T> ProductOfSubtractions3D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, ISubtractionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate3D<T, T, T, SubtractOperator<T>, ProductOperator<T>>(x, y);
+
+    public static ValueTuple<T, T, T, T> ProductOfSubtractions4D<T>(ReadOnlySpan<T> x, ReadOnlySpan<T> y)
+        where T : struct, IMultiplicativeIdentity<T, T>, ISubtractionOperators<T, T, T>, IMultiplyOperators<T, T, T>
+        => Aggregate4D<T, T, T, SubtractOperator<T>, ProductOperator<T>>(x, y);
+
 }
