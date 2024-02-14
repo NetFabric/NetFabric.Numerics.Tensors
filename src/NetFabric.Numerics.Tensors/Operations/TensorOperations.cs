855b0ea4e14a97b62269dec2a3222520827b608d
Antao Almada <antao.almada@gmail.com>
2024-02-14 20:15:51 +0000

    Add inline documentation to to the static classes

---
+++ src/NetFabric.Numerics.Tensors/Operations/TensorOperations.cs
@@ -0,0 +1,12 @@
+namespace NetFabric.Numerics.Tensors;
+
+/// <summary>
+/// Provides a set of predefined operations applicable to <see cref="ReadOnlySpan{T}"/>.
+/// </summary>
+/// <remarks>
+/// This static class encapsulates a collection of predefined operations that can be performed on <see cref="ReadOnlySpan{T}"/>.
+/// These operations include arithmetic operations (such as sum, product, minimum, maximum), as well as specialized operations like finding the minimum magnitude and simultaneous minimum and maximum.
+/// </remarks>
+public static partial class TensorOperations
+{
+}
--- src/NetFabric.Numerics.Tensors/Tensor.cs
+++ src/NetFabric.Numerics.Tensors/Tensor.cs
@@ -1,5 +1,13 @@
 namespace NetFabric.Numerics.Tensors;
 
+/// <summary>
+/// Provides methods to perform operations on <see cref="ReadOnlySpan{T}"/>.
+/// </summary>
+/// <remarks>
+/// This static class contains methods for operating on data represented by <see cref="ReadOnlySpan{T}"/>.
+/// The methods are categorized into two groups: element-wise operations (Apply) and aggregation operations (Aggregate).
+/// Hardware acceleration is utilized where available to optimize performance.
+/// </remarks>
 public static partial class Tensor
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
