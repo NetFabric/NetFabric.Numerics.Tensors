fbb49446182430c1692cbb8fb0eb28321d348d82
Antão Almada <antao.almada@farfetch.com>
2024-02-27 20:13:06 +0000

    Add IndexOf for unary and ternary delegates

--- src/NetFabric.Numerics.Tensors/Operations/IndexOf.cs
+++ src/NetFabric.Numerics.Tensors/Operations/IndexOf.cs
@@ -4,17 +4,17 @@ public static partial class TensorOperations
 {
     public static int IndexOfGreaterThan<T>(ReadOnlySpan<T> source, T value)
         where T : struct, IComparisonOperators<T, T, bool>
-        => Tensor.IndexOfPredicateBinary<T, GreaterThanAnyOperator<T>>(source, value);
+        => Tensor.IndexOfPredicate<T, GreaterThanAnyOperator<T>>(source, value);
 
     public static int IndexOfGreaterThanOrEqual<T>(ReadOnlySpan<T> source, T value)
         where T : struct, IComparisonOperators<T, T, bool>
-        => Tensor.IndexOfPredicateBinary<T, GreaterThanOrEqualAnyOperator<T>>(source, value);
+        => Tensor.IndexOfPredicate<T, GreaterThanOrEqualAnyOperator<T>>(source, value);
 
     public static int IndexOfLessThan<T>(ReadOnlySpan<T> source, T value)
         where T : struct, IComparisonOperators<T, T, bool>
-        => Tensor.IndexOfPredicateBinary<T, LessThanAnyOperator<T>>(source, value);
+        => Tensor.IndexOfPredicate<T, LessThanAnyOperator<T>>(source, value);
 
     public static int IndexOfLessThanOrEqual<T>(ReadOnlySpan<T> source, T value)
         where T : struct, IComparisonOperators<T, T, bool>
-        => Tensor.IndexOfPredicateBinary<T, LessThanOrEqualAnyOperator<T>>(source, value);
+        => Tensor.IndexOfPredicate<T, LessThanOrEqualAnyOperator<T>>(source, value);
 }
